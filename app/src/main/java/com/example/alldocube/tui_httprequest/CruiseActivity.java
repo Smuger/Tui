6d4d7c5b4af375430efce39aeb96d9e119de38da
Smuger <krzysiek.kwietniewski@gmail.com>
2017-11-12 03:11:18 +0000

    Cruise activity

--- app/src/main/java/com/example/alldocube/tui_httprequest/CruiseActivity.java
+++ app/src/main/java/com/example/alldocube/tui_httprequest/CruiseActivity.java
@@ -1,5 +1,7 @@
 package com.example.alldocube.tui_httprequest;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +10,8 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import org.apache.commons.io.IOUtils;
@@ -22,55 +26,71 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 public class CruiseActivity extends AppCompatActivity {
+    EditText editText = null;
+    ArrayList<Object> list = null;
+    public static int selectedid = 1;
+    Context x = this;
 
-    ArrayList <Object> list = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cruise);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        Log.d("asd",""+ShipActivity.selectedid);
-        try{
+        Log.d("asd", "" + ShipActivity.selectedid);
+        try {
 
-        String urlParameters  = "ID="+ShipActivity.selectedid;
+            String urlParameters = "ID=" + ShipActivity.selectedid;
 
-        byte[] postData       = urlParameters.getBytes( StandardCharsets.UTF_8 );
-        int    postDataLength = postData.length;
-        String request        = "https://venuscallipyge.nexttry.pl/wp-json/vcapi/getcruise";
-        URL url            = new URL( request );
-        HttpURLConnection conn= (HttpURLConnection) url.openConnection();
-        conn.setDoOutput( true );
-        conn.setInstanceFollowRedirects( false );
-        conn.setRequestMethod( "POST" );
-        conn.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
-        conn.setRequestProperty( "charset", "utf-8");
-        conn.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
-        conn.setUseCaches( false );
-        try( DataOutputStream wr = new DataOutputStream( conn.getOutputStream())) {
-            wr.write( postData );
+            byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
+            int postDataLength = postData.length;
+            String request = "https://venuscallipyge.nexttry.pl/wp-json/vcapi/getcruise";
+            URL url = new URL(request);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setInstanceFollowRedirects(false);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestProperty("charset", "utf-8");
+            conn.setRequestProperty("Content-Length", Integer.toString(postDataLength));
+            conn.setUseCaches(false);
+            try (DataOutputStream wr = new DataOutputStream(conn.getOutputStream())) {
+                wr.write(postData);
 
-        }
-        catch (Exception e) {
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            InputStream in = new BufferedInputStream(conn.getInputStream());
+            String result = IOUtils.toString(in, "UTF-8");
+            JSONObject jsonObject = new JSONObject(result);
+            Log.d("asd", "" + result);
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        InputStream in = new BufferedInputStream(conn.getInputStream());
-        String result = IOUtils.toString(in, "UTF-8");
-        JSONObject jsonObject = new JSONObject(result);
-            Log.d("asd",""+result);
-        }
+        Button mButton2 = (Button) findViewById(R.id.button9);
+        editText = (EditText) findViewById(R.id.editText);
+        mButton2.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (editText.getText().toString().length() == 6)
+                    changeActivity();
+            }
 
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+            private void changeActivity() {
+
+                Intent intent4 = new Intent(x, HarborActivity.class);
+                startActivity(intent4);
+
+            }
+        });
 
 // Create an ArrayAdapter using the string array and a default spinner layout
-    //    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-          //      R.array.planets_array, android.R.layout.simple_spinner_item);
+        //    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+        //      R.array.planets_array, android.R.layout.simple_spinner_item);
 // Specify the layout to use when the list of choices appears
-     //   adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //   adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Apply the adapter to the spinner
-       // spinner.setAdapter(adapter);
+        // spinner.setAdapter(adapter);
 
 
     }
